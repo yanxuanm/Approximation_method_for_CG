7572b47dc4a314a43793608ca8ca69d976a2e7f4
yanxuanm <joshuaktf@gmail.com>
2018-11-21 18:37:36 -0500

    analytic solution of RPS

--- One_Dimensional_Mass_Spring/OneD_SpringMass_RPS.m
+++ One_Dimensional_Mass_Spring/OneD_SpringMass_RPS.m
@@ -1,7 +1,9 @@
 clear; clc; close all; 
 
 syms y(t)
-
+% Exact analytical solution to 1-D spring-mass damper.
+% Try to manipulate these variables, where a is coefficient of velocity, 
+% m is mass, and k is stiffness of spring. 
 a = 0.05;
 m = 10;
 k = 5;
@@ -13,9 +15,19 @@ ySol(t) = dsolve(eqn, cond);
 fplot(ySol, [0, 250]);
 hold on
 
+% Approximate solution by asymptotic expansion. 
+% NOTE: need to recover dimention and units of each dimensionaless terms. 
 epsilon = a/(2*sqrt(m*k))
-
 v(t) = cos(t/sqrt(m/k))+epsilon*(sin(t/sqrt(m/k))-t*cos(t/sqrt(m/k)))...
     +epsilon^2*((t/sqrt(m/k))^2/2*cos(t/sqrt(m/k))-(t/sqrt(m/k))/2*sin(t/sqrt(m/k)))
 
 fplot(v, [0, 250]);
+
+% This RPS gives a very good approximation when epsilon is much less than
+% 1. The main effect of small damping is to reduce the amplitude of the  
+% oscillation exponentially in time. The frequency shift is only important 
+% once eplison^2*t ? 1, and on that long time the amplitude of the residual
+% oscillation is exponentially small (? exp(?1/2*epsilon)). So we don?t 
+% worry too much about the frequency shift. However, if time is arbitrarily
+% large, or epsilon is large, RPS will fail. Slowly accumulated error is called
+% secular error.
