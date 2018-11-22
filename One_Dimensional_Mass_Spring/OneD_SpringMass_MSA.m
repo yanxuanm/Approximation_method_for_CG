be6e74602a97d2abe17059773bec85b1bf44ee33
yanxuanm <joshuaktf@gmail.com>
2018-11-22 16:03:11 -0500

    delete unuseful comments

--- One_Dimensional_Mass_Spring/OneD_SpringMass_MSA.m
+++ One_Dimensional_Mass_Spring/OneD_SpringMass_MSA.m
@@ -26,11 +26,3 @@ fplot(v, [0, 150]);
 xlabel('Time')
 ylabel('Displacement')
 legend('Exact Solution','MSA')
-% This RPS gives a very good approximation when epsilon is much less than 1.
-% The main effect of small damping is to reduce the amplitude of the  
-% oscillation exponentially in time. The frequency shift is only important 
-% once eplison^2*t ~ 1, and on that long time the amplitude of the residual
-% oscillation is exponentially small (~exp(-1/2*epsilon)). So we don't 
-% worry too much about the frequency shift. However, if time is arbitrarily
-% large, or epsilon is large, RPS will fail. Slowly accumulated error is called
-% secular error.
