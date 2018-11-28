08eb2a5485d6022cfdb070f1277d8c1bf9d37183
yanxuanm <joshuaktf@gmail.com>
2018-11-27 19:55:36 -0500

    add github md sty

--- One_Dimensional_Mass_Spring/Nonlinear_Spring_RPS.m
+++ One_Dimensional_Mass_Spring/Nonlinear_Spring_RPS.m
@@ -11,6 +11,8 @@ second_diff_pert_series = diff(first_diff_pert_series, t);
 % Now substitute above equation to the nonlinear equation
 combine = second_diff_pert_series + E*first_diff_pert_series + pert_series^2==0 
 
+% collect function returns an equation which is nicely ordered according to
+% different power of E. 
 combine = collect(combine, E);
 equation = strcat("""", string(combine), """")
 E0 = strcat(" """, "E^0", """");
@@ -19,6 +21,9 @@ E2 = strcat(" """, "E^2", """");
 
 E_command = [E0, E1, E2];
 pert_term = sym(size(E_command))
+
+% This for loop using python to collect different perturbation term
+% according to the power of E. 
 for i=1:3
     commandStr = strcat("python collect.py ", equation, E_command(i));
     [status, commandOut] = system(commandStr);
