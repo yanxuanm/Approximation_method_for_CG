6de915fa28c2520a96cb2c0b5cf726fe2221d73f
joshua <joshuaktf@gmail.com>
2018-11-29 01:15:44 -0500

    build non-linear solver

--- One_Dimensional_Mass_Spring/Nonlinear_Spring_RPS.m
+++ One_Dimensional_Mass_Spring/Nonlinear_Spring_RPS.m
@@ -20,7 +20,7 @@ E1 = strcat(" """, "E^1", """");
 E2 = strcat(" """, "E^2", """");
 
 E_command = [E0, E1, E2];
-pert_term = sym(size(E_command))
+sym pert_term
 
 % This for loop using python to collect different perturbation term
 % according to the power of E. 
@@ -33,10 +33,83 @@ end
 
 V = odeToVectorField(pert_term(1));
 F = matlabFunction(V,'vars',{'t','Y'});
-sol = ode45(F,[0 4],[1 0]);
-x = linspace(0,4,10);
-y = deval(sol,x,2);
-plot(x,y);
+sol = ode45(F,[0 3],[1 0]);
+x = linspace(0,3,20);
+first_y = deval(sol,x,1);
+first_diffy = deval(sol,x,2);
+plot(x,first_y);
 
 
+% dim_matrix = zeros(size(pert_term, 2), size(sol.y, 2))
+% temp_pert_term = sym(size(dim_matrix))
+sym replace_y0
+for i = 1:size(first_y, 2)
+    replace_y0(i) = subs(pert_term(2), diff(y0(t), t), first_diffy(i))
+end
 
+
+sym second_per
+for i = 1:size(replace_y0, 2)
+    second_per(i) = subs(replace_y0(i), y0(t), first_y(i))
+end
+
+Y2 = zeros(size(first_y));
+diff_Y2 = zeros(size(first_y));
+for i = 1:size(second_per, 2)
+    
+    V2 = odeToVectorField(second_per(i));
+    F2 = matlabFunction(V2,'vars',{'t','Y'});
+    sol2 = ode45(F2,[0 3],[1 0]);
+    x = linspace(0,3,20);
+    Y2(i, :) = deval(sol2,x,1);
+    diff_Y2(i, :) = deval(sol2,x,2);
+
+end
+figure(2)
+plot(x,diag(Y2));
+
+second_y = diag(Y2);
+second_diffy = diag(diff_Y2);
+
+sym replace_y1
+for i = 1:size(first_y, 2)
+    replace_y1(i) = subs(pert_term(3), diff(y1(t), t), second_diffy(i))
+end
+
+sym replace_y1_y0
+for i = 1:size(first_y, 2)
+    replace_y1_y0(i) = subs(replace_y1(i), y0(t), first_y(i))
+end
+
+sym third_per
+for i = 1:size(replace_y0, 2)
+    third_per(i) = subs(replace_y1_y0(i), y1(t), second_y(i))
+end
+
+Y3 = zeros(size(first_y));
+diff_Y3 = zeros(size(first_y));
+for i = 1:size(third_per, 2)
+    
+    V3 = odeToVectorField(third_per(i));
+    F3 = matlabFunction(V3,'vars',{'t','Y'});
+    sol3 = ode45(F3,[0 3],[1 0]);
+    x = linspace(0,3,20);
+    Y3(i, :) = deval(sol3,x,1);
+    diff_Y3(i, :) = deval(sol3,x,2);
+
+end
+figure(3)
+plot(x,diag(Y3));
+
+third_y = diag(Y3);
+third_diffy = diag(diff_Y3);
+
+epsilon = 0.001
+
+ret = first_y' + epsilon.*second_y + epsilon^2.*third_y;
+figure(4)
+plot(x, ret)
+
+diffret = first_diffy' + epsilon.*second_diffy + epsilon^2.*third_diffy;
+figure(5)
+plot(x, diffret)
